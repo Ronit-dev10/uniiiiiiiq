c7250348db4d7dd1f1617efb410fe14981fb0d4a
Ronit-dev10 <ronitvig@gmail.com>
2025-08-16 19:13:09 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { createServer } from "./server";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // <-- important for Vercel SPA relative paths
   server: {
     host: "::",
     port: 8080,
@@ -28,11 +28,9 @@ export default defineConfig(({ mode }) => ({
 function expressPlugin(): Plugin {
   return {
     name: "express-plugin",
-    apply: "serve", // Only apply during development (serve mode)
+    apply: "serve", // Only applies in development
     configureServer(server) {
       const app = createServer();
-
-      // Add Express app as middleware to Vite dev server
       server.middlewares.use(app);
     },
   };
