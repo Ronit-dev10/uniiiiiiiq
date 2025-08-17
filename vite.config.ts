ece02056e5d53f6efccdc42f24226a0a56b4caf8
Ronit-dev10 <ronitvig@gmail.com>
2025-08-16 19:21:24 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,37 @@
 import { defineConfig, Plugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { createServer } from "./server";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "./", // important for Vercel SPA relative paths
+export default defineConfig(({ mode }) => ({
+  base: "./", // important for SPA on Vercel
+  server: {
+    host: "::",
+    port: 8080,
+    fs: {
+      allow: ["./client", "./shared"],
+      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
+    },
+  },
   build: {
     outDir: "dist/spa",
   },
-  plugins: [react()],
+  plugins: [react(), expressPlugin()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client"),
       "@shared": path.resolve(__dirname, "./shared"),
     },
   },
-});
+}));
+
+function expressPlugin(): Plugin {
+  return {
+    name: "express-plugin",
+    apply: "serve", // only in dev mode, ignored in production
+    configureServer(server) {
+      const app = createServer();
+      server.middlewares.use(app);
+    },
+  };
+}
