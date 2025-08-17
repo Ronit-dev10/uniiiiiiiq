645edc91c0a99bd5ed06aa262a1b1e2e0e1e0584
Ronit-dev10 <ronitvig@gmail.com>
2025-08-16 19:50:43 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,17 @@
-import { defineConfig, Plugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { createServer } from "./server";
 
-export default defineConfig(({ mode }) => ({
-  base: "./", // important for SPA assets
-  server: {
-    host: "::",
-    port: 8080,
-    fs: {
-      allow: ["./client", "./shared"],
-      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"]
-    }
-  },
-  build: {
-    outDir: "dist/spa"
-  },
-  plugins: [react(), expressPlugin()],
+export default defineConfig({
+  base: "./",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client"),
       "@shared": path.resolve(__dirname, "./shared")
     }
+  },
+  build: {
+    outDir: "dist/spa"
   }
-}));
-
-function expressPlugin(): Plugin {
-  return {
-    name: "express-plugin",
-    apply: "serve", // dev mode only
-    configureServer(server) {
-      const app = createServer();
-      server.middlewares.use(app);
-    }
-  };
-}
+});
